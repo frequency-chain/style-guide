3afb1f3fe3135ad89fd7e938808fd73673e958a3
Claire Olmstead <olmsteadclaire@gmail.com>
2024-09-19 14:49:51 -0700

    add index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+declare module 'frequency-style-guide';
+
+declare module 'frequency-style-guide/tailwind.config.js';
+
+declare module '*.svelte' {
+  const component: any;
+  export default component;
+}
