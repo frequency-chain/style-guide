7ef11cb151716346c95fff5c7a1ac86329238efc
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-02 15:58:54 -0700

    add select to index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './atoms/Button.svelte';
+export { default as Select } from './atoms/Select.svelte';
 export { default as Typography } from './typography/Typography.svelte';
 import '../styles/index.css';
