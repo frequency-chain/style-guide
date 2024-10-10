3259b35d60dfb6f81ad63a4a499f0b22d8a01f92
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-10 11:59:45 -0700

    add accordion, accorfion list, and icon stories

--- src/lib/shadcnComponents/ui/select/select-trigger.svelte
+++ src/lib/shadcnComponents/ui/select/select-trigger.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Select as SelectPrimitive } from 'bits-ui';
-  import Chevron from '../../../../assets/Chevron.svelte';
-  import Error from '../../../../assets/Error.svelte';
+  import Chevron from '../../../../icons/Chevron.svelte';
+  import Error from '../../../../icons/Error.svelte';
   import { cn } from '../../../../utils';
 
   export let error: string | undefined;
@@ -16,7 +16,7 @@
 <div class="flex items-center gap-3">
   <SelectPrimitive.Trigger
     class={cn(
-      'flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 aria-[invalid]:border-destructive [&>span]:line-clamp-1 data-[placeholder]:[&>span]:text-muted-foreground',
+      'border-input bg-background ring-offset-background focus-visible:ring-ring aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       error ? 'border-2 border-error' : 'border border-gray3',
       className
     )}
