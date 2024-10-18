7a3827af99d247af9e4197a73317d2c70f48a060
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-10-17 17:40:20 -0700

    Refs/heads/6 accordion (#24)
    * add accordion, accorfion list, and icon stories
    
    * update error
    
    * update
    
    ---------
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

---
+++ src/lib/features/Accordion.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+  import Accordion from './Accordion.svelte';
+</script>
+
+<Meta title="Design System/Features/Accordion" component={Accordion} />
+
+<!-- Primary -->
+<Story name="Accordion">
+  <Accordion trigger="Hello world." content="Hello there." />
+</Story>
