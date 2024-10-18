7c14454969f5bb0a07793536571644e7657ced67
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-10-17 18:16:04 -0700

    spacing story (#26)
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

--- src/lib/shadcnComponents/ui/select/select-trigger.svelte
+++ src/lib/shadcnComponents/ui/select/select-trigger.svelte
@@ -4,31 +4,33 @@
   import Error from '../../../../icons/Error.svelte';
   import { cn } from '../../../../utils';
 
-  export let error: string | undefined;
+    export let error: string | undefined;
 
-  type $$Props = SelectPrimitive.TriggerProps & { error: string | undefined };
-  type $$Events = SelectPrimitive.TriggerEvents;
+    type $$Props = SelectPrimitive.TriggerProps & { error: string | undefined };
+    type $$Events = SelectPrimitive.TriggerEvents;
 
-  let className: $$Props['class'] = undefined;
-  export { className as class };
+    let className: $$Props['class'] = undefined;
+    export {className as class};
 </script>
 
-<div class="flex items-center gap-3">
+<div class="flex items-center gap-f12">
   <SelectPrimitive.Trigger
     class={cn(
       'border-input bg-background ring-offset-background focus-visible:ring-ring aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       error ? 'border-2 border-error' : 'border border-gray3',
       className
     )}
-    {...$$restProps}
-    let:builder
-    on:click
-    on:keydown
-  >
-    <slot {builder} />
-    <div class="h-4 w-4">
-      <Chevron />
-    </div>
-  </SelectPrimitive.Trigger>
-  {#if error}<Error />{/if}
+            {...$$restProps}
+            let:builder
+            on:click
+            on:keydown
+    >
+        <slot {builder}/>
+        <div class="h-4 w-4">
+            <Chevron/>
+        </div>
+    </SelectPrimitive.Trigger>
+    {#if error}
+        <Error/>
+    {/if}
 </div>
