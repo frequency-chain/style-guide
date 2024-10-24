8bf3b263458e324275d2a7844e56123bb242aba2
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-24 14:26:30 -0700

    input element

--- src/lib/shadcnComponents/ui/select/select-trigger.svelte
+++ src/lib/shadcnComponents/ui/select/select-trigger.svelte
@@ -1,33 +1,33 @@
 <script lang="ts">
-  import { Select as SelectPrimitive } from 'bits-ui';
-  import { cn } from '../../../../utils/utils';
-  import { Error, Chevron } from '../../../assets/index';
+    import {Select as SelectPrimitive} from 'bits-ui';
+    import {cn} from '../../../../utils/utils';
+    import {Error, Chevron} from '../../../assets/index';
 
-  export let error: string | undefined;
+    export let error: string | undefined;
 
-  type $$Props = SelectPrimitive.TriggerProps & { error: string | undefined };
-  type $$Events = SelectPrimitive.TriggerEvents;
+    type $$Props = SelectPrimitive.TriggerProps & { error: string | undefined };
+    type $$Events = SelectPrimitive.TriggerEvents;
 
-  let className: $$Props['class'] = undefined;
-  export { className as class };
+    let className: $$Props['class'] = undefined;
+    export {className as class};
 </script>
 
 <div class="flex items-center gap-f12">
-  <SelectPrimitive.Trigger
-    class={cn(
-      'border-input bg-background ring-offset-background focus-visible:ring-ring aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+    <SelectPrimitive.Trigger
+            class={cn(
+      'border-input bg-cream ring-offset-background focus-visible:ring-ring aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border px-3 py-2 sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       error ? 'border-2 border-error' : 'border border-gray3',
       className
     )}
-    {...$$restProps}
-    let:builder
-    on:click
-    on:keydown
-  >
-    <slot {builder} />
-    <Chevron class="h-4 w-4" />
-  </SelectPrimitive.Trigger>
-  {#if error}
-    <Error />
-  {/if}
+            {...$$restProps}
+            let:builder
+            on:click
+            on:keydown
+    >
+        <slot {builder}/>
+        <Chevron class="h-4 w-4"/>
+    </SelectPrimitive.Trigger>
+    {#if error}
+        <Error/>
+    {/if}
 </div>
