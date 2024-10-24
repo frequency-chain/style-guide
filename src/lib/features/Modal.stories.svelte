2f5c5b4eb4dd15beccc79af8ebb1ef483d1ac296
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-23 18:57:09 -0700

    modal component and story initialized

---
+++ src/lib/features/Modal.stories.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+  import Modal from './Modal.svelte';
+</script>
+
+<Meta title="UI Components/Features/Modal" component={Modal} />
+
+<!--Primary-->
+<Story name="Modal">
+  <Modal />
+</Story>
---
+++ src/lib/features/Modal.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Button from '../atoms/Button.svelte';
+  import { Root, Trigger, Content, Header, Title, Description } from '../shadcnComponents/ui/dialog';
+</script>
+
+<Root>
+  <Trigger><Button>Open</Button></Trigger>
+  <Content>
+    <Header>
+      <Title>Are you sure absolutely sure?</Title>
+      <Description>
+        This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+      </Description>
+    </Header>
+  </Content>
+</Root>
