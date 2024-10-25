3b90b4014e6bad30d3287ed5c1ac095c4a49c2d1
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-25 12:26:55 -0700

    format

--- src/lib/atoms/Input.svelte
+++ src/lib/atoms/Input.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { cn } from '../../utils/utils';
-  import { Error } from '../assets';
+  import FormElement from './FormElement.svelte';
 
   export let label = '';
   export let isRequired = false;
@@ -8,39 +8,17 @@
   export let value = '';
   export let placeholder = 'Enter some text...';
   export let error: string | undefined;
-
-  function handleInput(event) {
-    value = event.target.value;
-  }
 </script>
 
-<div class="flex flex-col gap-f4">
-  <label class="form-item-label">
-    {label}
-    {#if isRequired}
-      <span class="text-error">*</span>
-    {/if}
-  </label>
-  {#if description}
-    <span class="form-item-description">{description}</span>
-  {/if}
-  <div class="flex items-center gap-f12">
-    <input
-      class={cn(
-        'border-input aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground sm flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border bg-cream px-3 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
-        error ? 'border-2 border-error' : 'border border-gray3',
-        $$restProps.class
-      )}
-      type="text"
-      {placeholder}
-      on:input={handleInput}
-      bind:value
-    />
-    {#if error}
-      <Error />
-    {/if}
-  </div>
-  {#if error}
-    <span class="form-error">{error}</span>
-  {/if}
-</div>
+<FormElement {label} {isRequired} {description} {value} {placeholder} {error}>
+  <input
+    class={cn(
+      'border-input aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground sm flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border bg-cream px-3 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      error ? 'border-2 border-error' : 'border border-gray3',
+      $$restProps.class
+    )}
+    type="text"
+    {placeholder}
+    bind:value
+  />
+</FormElement>
--- src/lib/atoms/Textarea.svelte
+++ src/lib/atoms/Textarea.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { cn } from '../../utils/utils';
-  import { Error } from '../assets/index';
+  import FormElement from './FormElement.svelte';
 
   export let label = '';
   export let isRequired = false;
@@ -10,34 +10,13 @@
   export let error: string | undefined;
 </script>
 
-<div class="flex flex-col gap-f4">
-  <label class="form-item-label">
-    {label}
-    {#if isRequired}
-      <span class="text-error">*</span>
-    {/if}
-  </label>
-
-  {#if description}
-    <span class="form-item-description">{description}</span>
-  {/if}
-
-  <div class="flex items-center gap-f12">
-    <textarea
-      class={cn(
-        'border-input aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground sm flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border bg-cream px-3 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
-        error ? 'border-2 border-error' : 'border border-gray3'
-      )}
-      {placeholder}
-      bind:value
-    ></textarea>
-
-    {#if error}
-      <Error />
-    {/if}
-  </div>
-
-  {#if error}
-    <span class="form-error">{error}</span>
-  {/if}
-</div>
+<FormElement {label} {isRequired} {description} {value} {placeholder} {error}>
+  <textarea
+    class={cn(
+      'border-input aria-[invalid]:border-destructive data-[placeholder]:[&>span]:text-muted-foreground sm flex h-10 w-full max-w-[388px] items-center justify-between rounded-md border bg-cream px-3 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      error ? 'border-2 border-error' : 'border border-gray3'
+    )}
+    {placeholder}
+    bind:value
+  ></textarea>
+</FormElement>
