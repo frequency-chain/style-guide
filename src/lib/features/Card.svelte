90de0e960dfa682641149029a49c5d58508c3f0c
shannonwells <shannonwells@users.noreply.github.com>
2024-10-24 17:57:58 -0700

    tidy up

--- src/lib/features/Card.svelte
+++ src/lib/features/Card.svelte
@@ -5,13 +5,13 @@
   export let content = 'This is default content';
   export let bgColor = 'cream';
   export let color = 'black'
-  export let headerColor = 'navy'
+  export let titleColor = 'navy'
   export let icon = UserFilled;
 </script>
 
 <div class="p-6 card-shadow rounded-xl bg-{bgColor} {$$restProps.class}">
   <div class="pb-6 flex justify-between">
-    <h5 class="font-bold text-{headerColor} inline">{title}</h5>
+    <h5 class="font-bold text-{titleColor} inline">{title}</h5>
     <svelte:component this={icon} class="w-f48 h-f48"/>
   </div>
   <div class="text-{color}">
