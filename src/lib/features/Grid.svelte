829c8379ae38ce293ba01091560f54f6b2087510
shannonwells <shannonwells@users.noreply.github.com>
2024-10-28 14:17:00 -0700

    grid component

---
+++ src/lib/features/Grid.svelte
@@ -0,0 +1,7 @@
+<style>
+</style>
+<div class="m-4 md:m-8 lg:m-32 {$$restProps.class}">
+<div class="grid grid-cols-4 gap-4 sm:grid-cols-4 md:gap-8 md:grid-cols-6 lg:grid-cols-12 lg:gap-8">
+  <slot />
+</div>
+</div>
