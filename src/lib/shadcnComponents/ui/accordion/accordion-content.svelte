0ccce2336805136281031266fc720be8e602bfd6
Claire Olmstead <olmsteadclaire@gmail.com>
2024-10-28 13:45:20 -0700

    fix paths

--- src/lib/shadcnComponents/ui/accordion/accordion-content.svelte
+++ src/lib/shadcnComponents/ui/accordion/accordion-content.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  import { Accordion as AccordionPrimitive } from 'bits-ui';
-  import { slide } from 'svelte/transition';
-  import { cn } from '$lib/utils/utils';
+    import {Accordion as AccordionPrimitive} from 'bits-ui';
+    import {slide} from 'svelte/transition';
+    import {cn} from '../utils/utils';
 
-  type $$Props = AccordionPrimitive.ContentProps;
+    type $$Props = AccordionPrimitive.ContentProps;
 
-  let className: $$Props['class'] = undefined;
-  export let transition: $$Props['transition'] = slide;
-  export let transitionConfig: $$Props['transitionConfig'] = {
-    duration: 200,
-  };
-  export { className as class };
+    let className: $$Props['class'] = undefined;
+    export let transition: $$Props['transition'] = slide;
+    export let transitionConfig: $$Props['transitionConfig'] = {
+        duration: 200,
+    };
+    export {className as class};
 </script>
 
 <AccordionPrimitive.Content
-  class={cn('sm overflow-hidden transition-all', className)}
-  {transition}
-  {transitionConfig}
-  {...$$restProps}
+        class={cn('sm overflow-hidden transition-all', className)}
+        {transition}
+        {transitionConfig}
+        {...$$restProps}
 >
-  <div class="pb-4 pt-0 text-normal">
-    <slot />
-  </div>
+    <div class="pb-4 pt-0 text-normal">
+        <slot/>
+    </div>
 </AccordionPrimitive.Content>
--- src/lib/shadcnComponents/ui/dialog/dialog-title.svelte
+++ src/lib/shadcnComponents/ui/dialog/dialog-title.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import { Dialog as DialogPrimitive } from 'bits-ui';
-  import { cn } from '$lib/utils/utils.js';
+    import {Dialog as DialogPrimitive} from 'bits-ui';
+    import {cn} from '../utils/utils.js';
 
-  type $$Props = DialogPrimitive.TitleProps;
+    type $$Props = DialogPrimitive.TitleProps;
 
-  let className: $$Props['class'] = undefined;
-  export { className as class };
+    let className: $$Props['class'] = undefined;
+    export {className as class};
 </script>
 
 <DialogPrimitive.Title class={cn('lg font-bold text-navy', className)} {...$$restProps}>
-  <slot />
+    <slot/>
 </DialogPrimitive.Title>
