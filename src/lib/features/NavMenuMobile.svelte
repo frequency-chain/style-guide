264337590231f5546623436f79f2203d169a7642
Wil Wade <wil.wade@projectliberty.io>
2024-11-07 13:08:37 -0500

    Fixes for mobile nav menu

--- src/lib/features/NavMenuMobile.svelte
+++ src/lib/features/NavMenuMobile.svelte
@@ -11,7 +11,9 @@
 </script>
 
 <input type="checkbox" class="peer hidden" bind:checked={isOpen} id={toggleIdentifier} />
-<OpenClose classes="stroke-black hover:stroke-teal flex self-center lg:hidden" />
+<OpenClose
+  classes="stroke-navy peer-checked:stroke-white peer-checked:hover:stroke-teal hover:stroke-teal flex self-center lg:hidden"
+/>
 <div
   class="z-1 fixed right-0 top-0 z-10 flex h-0 w-[100vw] flex-col justify-between gap-f24 overflow-y-scroll bg-navy px-[12%] transition-[height] duration-[1s] peer-checked:h-[100vh] lg:hidden"
   aria-expanded={isOpen}
@@ -19,14 +21,14 @@
   <nav id="mobile-navigation" aria-label="Main" class="mt-[60px] flex flex-col gap-4 text-white">
     {#each menuItems as item}
       {#if item.isButton}
-        <Button size="full" class="mx-auto">
+        <Button size="full" class="max-w-[360px] self-center">
           <a href={item.href} target={item.isExternal ? '_blank' : '_self'}>{item.label}</a>
         </Button>
       {:else}
         <a
           href={item.href}
           target={item.isExternal ? '_blank' : '_self'}
-          class={`h6 underline-on-hover font-bold after:bg-current`}>{item.label}</a
+          class={`h4 underline-on-hover font-bold after:bg-current`}>{item.label}</a
         >
       {/if}
     {/each}
