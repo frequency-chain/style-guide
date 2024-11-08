57b72dfee478a443a6d8041bb5f8f332af737d93
Joe Caputo <joseph.caputo@projectliberty.io>
2024-11-08 11:35:44 -0500

    feat: allow Footer component style overrides

--- src/lib/features/Footer.svelte
+++ src/lib/features/Footer.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import { FrequencyLogo, XLogo, DiscordLogo } from '../assets';
   import IconButton from '../atoms/IconButton.svelte';
+  import { cn } from '../utils/utils';
+
+  export let classes: string = '';
 </script>
 
-<footer class="sm md:md flex flex-col items-center gap-f16 border-t border-black py-f48 md:gap-f24">
+<footer class={cn('sm md:md flex flex-col items-center gap-f16 border-t border-black py-f48 md:gap-f24', classes)}>
   <div class="flex gap-f40">
     <a href="/privacy" class="underline transition duration-[0.3s] hover:text-brightBlue">Privacy Policy</a>
-    <a href="/terms" class="underline transition duration-[0.3s] hover:text-brightBlue">Terms of Conditions</a>
+    <a href="/terms" class="underline transition duration-[0.3s] hover:text-brightBlue">Terms and Conditions</a>
   </div>
   <FrequencyLogo class="w-[146px] md:w-[257px]" />
-  <aside>{`© ${new Date().getFullYear()} Frequency Network Foundation. All Right Reserved`}</aside>
+  <aside>{`© ${new Date().getFullYear()} Frequency Network Foundation. All Rights Reserved.`}</aside>
   <div class="flex gap-f16" aria-label="Social Links">
     <IconButton label="X/Twitter" href={'https://twitter.com/one_frequency'}>
       <XLogo />
