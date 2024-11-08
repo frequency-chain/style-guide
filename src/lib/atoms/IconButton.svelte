e8bae88ea77443b9a7d3012dcf81dcb05ed1039a
Wil Wade <wil.wade@projectliberty.io>
2024-11-08 12:57:34 -0500

    Blocked: Footer updates (#48)
    Updates to the footer to match the new styles
    
    # Solution
    
    - Add light/dark options to the footer
    - Remove the terms
    - Added option to override the privacy policy link
    ---------
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

--- src/lib/atoms/IconButton.svelte
+++ src/lib/atoms/IconButton.svelte
@@ -4,19 +4,31 @@
   export let label: string = '';
   export let href: string = '';
   export let isRound = false;
+  export let type: 'light' | 'dark' = 'light';
+
+  let iconBgColor = {
+    light: 'bg-navy',
+    dark: 'bg-white',
+  }[type];
+
+  let iconFgColor = {
+    light: 'text-white',
+    dark: 'text-black',
+  }[type];
 </script>
 
 <a
   aria-label={label}
   class={cn(
-    `flex h-f32 w-f32 items-center justify-center bg-black p-1 transition-colors duration-[0.3s] hover:bg-brightBlue`,
+    'flex h-f32 w-f32 items-center justify-center p-1 transition-colors duration-[0.3s] hover:bg-teal',
     isRound ? 'rounded-full' : 'rounded',
+    iconBgColor,
     $$restProps.class
   )}
   {href}
   target="_blank"
 >
-  <div class="h-auto w-full text-white">
+  <div class="h-auto w-full {iconFgColor}">
     <slot />
   </div>
 </a>
