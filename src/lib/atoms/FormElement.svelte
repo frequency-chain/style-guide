e2f9d88cd740b6d6376f0276bacc62aa6ced5dae
Claire Olmstead <olmsteadclaire@gmail.com>
2024-11-26 10:00:01 -0800

    respond to comments

--- src/lib/atoms/FormElement.svelte
+++ src/lib/atoms/FormElement.svelte
@@ -5,10 +5,13 @@
   export let isRequired = false;
   export let description = '';
   export let error: string | undefined;
+
+  const labelNoSpaces = label.split(' ').join('');
+  const id = `form-element-${labelNoSpaces}`;
 </script>
 
 <div class="flex flex-col gap-f4">
-  <label class="form-item-label" for="form-element">
+  <label class="form-item-label" for={id}>
     {label}
     {#if isRequired}
       <span class="text-error">*</span>
@@ -19,7 +22,7 @@
     <span class="form-item-description">{description}</span>
   {/if}
 
-  <div class="flex items-center gap-f12" id="form-element">
+  <div class="flex items-center gap-f12" {id}>
     <slot />
 
     {#if error}
