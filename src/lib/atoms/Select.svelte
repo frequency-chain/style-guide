3433352fa9a30ba65e9c89fa7adbada4cae84424
Claire Olmstead <olmsteadclaire@gmail.com>
2025-07-25 11:40:16 -0700

    add div around select so spacing doesn't get messed up

--- src/lib/atoms/Select.svelte
+++ src/lib/atoms/Select.svelte
@@ -28,20 +28,22 @@
   }: Props = $props();
 </script>
 
-<Root {...rest} {onSelectedChange}>
-  <Label {isRequired}>{label}</Label>
-  {#if description}
-    <span class="form-item-description">{description}</span>
-  {/if}
-  <Trigger {error} {isLoading}>
-    <Value class="text-nowrap overflow-ellipsis" {placeholder} />
-  </Trigger>
-  <Content class="border-gray3 border">
-    {#each options as option (option.value)}
-      <Item value={option.value}>{option.optionLabel}</Item>
-    {/each}
-  </Content>
-  {#if error}
-    <span class="form-error">{error}</span>
-  {/if}
-</Root>
+<div>
+  <Root {...rest} {onSelectedChange}>
+    <Label {isRequired}>{label}</Label>
+    {#if description}
+      <span class="form-item-description">{description}</span>
+    {/if}
+    <Trigger {error} {isLoading}>
+      <Value class="text-nowrap overflow-ellipsis" {placeholder} />
+    </Trigger>
+    <Content class="border-gray3 border">
+      {#each options as option (option.value)}
+        <Item value={option.value}>{option.optionLabel}</Item>
+      {/each}
+    </Content>
+    {#if error}
+      <span class="form-error">{error}</span>
+    {/if}
+  </Root>
+</div>
