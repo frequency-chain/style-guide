31573265366bc650879e6921ee79035f63d8ab44
Claire Olmstead <olmsteadclaire@gmail.com>
2025-07-30 07:52:14 -0700

    wip

--- src/lib/design-system/assets/Assets.stories.svelte
+++ src/lib/design-system/assets/Assets.stories.svelte
@@ -51,14 +51,14 @@
   let isOpen = $state(false);
 </script>
 
-<Story name="Icons">
+<!-- <Story name="Icons">
   <div class="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
     {#each Object.entries(icons) as [name, Icon] (name)}
       <div class="flex flex-col items-start justify-start p-4">
         <h2 class="mb-2 text-lg font-semibold">{name}</h2>
         {#if Icon === OpenClose}
           <input type="checkbox" id="mobile-menu-toggle" class="peer hidden" bind:checked={isOpen} />
-          <aside>Click it!</aside>
+          <aside>Click it</aside>
           <Icon toggleIdentifier="mobile-menu-toggle" class="mb-1 h-8 w-auto" bind:isOpen isDemo={true} />
         {:else}
           <Icon class="mb-1 h-8 w-auto" isDemo={true} />
@@ -66,9 +66,9 @@
       </div>
     {/each}
   </div>
-</Story>
+</Story> -->
 
-<Story name="Logos">
+<!-- <Story name="Logos">
   <div class="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
     {#each Object.entries(logos) as [name, Logo] (name)}
       <div class="flex flex-col items-start justify-start p-4">
@@ -77,4 +77,4 @@
       </div>
     {/each}
   </div>
-</Story>
+</Story> -->
